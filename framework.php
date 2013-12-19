1ad48b0b5b0e67c8a9a732da8f754efb89b1c3ca
guanweijia <guanweijia@babytree-inc.com>
2013-12-19 19:43:36 +0800

    add some code: singleton module

--- framework.php
+++ framework.php
@@ -4,6 +4,20 @@
  * @author
  */
 class FrontController {
+	protected $_controller, $_action, $_params, $_body;
+	static $_instance;
+
+	public static function getInstance() {
+		if ( !(self::$_instance instanceof self) ) {
+			self::$_instance = new self();
+		}
+	}
+
+	private function __construct() {
+	
+	}
+
+
 
 
 }
